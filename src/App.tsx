a09c9cc8b4e9487d571ea4f2cfa7be0714af51e2
Danny Julca Carhuaz <danny@email.djc.pe>
2025-08-05 07:45:37 -0500

    feat: move player

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,35 @@ interface Position {
   y: number;
 }
 
-import './App.css'
-
 function App() {
+  const [playerPos, setPlayerPos] = useState<Position>({ x: 20, y: 20 });
+  const playerPosRef = useRef<Position>({ x: 20, y: 20 });
+  const playerTargetRef = useRef<Position>({ x: 20, y: 20 });
+  const npcPosRef = useRef<Position>({ x: 0, y: 200 });
   const [npcPos, setNpcPos] = useState<Position>({ x: 0, y: 200 });
-  const direction = useRef<number>(1);  // 1: derecha, -1: izquierda
-  const requestRef = useRef<number | null>(null);
+
+  const direction = useRef<number>(1);
+  const requestRef = useRef<number>(0);
+  const speed = 2;
 
   const animate = () => {
-    setNpcPos((pos) => {
-      let newX = pos.x + 2 * direction.current;
+    npcPosRef.current.x += 2 * direction.current;
+    if (npcPosRef.current.x >= 350) direction.current = -1;
+    if (npcPosRef.current.x <= 0) direction.current = 1;
+    setNpcPos({ ...npcPosRef.current });
 
-      // Cambiar de dirección si choca con los bordes
-      if (newX >= 350) direction.current = -1;
-      if (newX <= 0) direction.current = 1;
+    const dx = playerTargetRef.current.x - playerPosRef.current.x;
+    const dy = playerTargetRef.current.y - playerPosRef.current.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
 
-      return { ...pos, x: newX };
-    });
+    if (distance > 1) {
+      const dirX = dx / distance;
+      const dirY = dy / distance;
+      playerPosRef.current.x += dirX * speed;
+      playerPosRef.current.y += dirY * speed;
+
+      setPlayerPos({ ...playerPosRef.current });
+    }
 
     requestRef.current = requestAnimationFrame(animate);
   };
@@ -39,12 +51,10 @@ function App() {
   const handleClick = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
     const svg = e.currentTarget;
     const rect = svg.getBoundingClientRect();
-
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    // setPlayerPos({ x, y });
-    console.log("position:",x,y)
+    playerTargetRef.current = { x, y };
   };
 
   return (
@@ -58,11 +68,12 @@ function App() {
           onClick={handleClick}
         >
           <rect x="0" y="0" width="400" height="400" fill="#000" />
-          <image href="/player.png" x={npcPos.x} y={npcPos.y} width="32" height="32" />
+          <image href="/player.png" x={playerPos.x} y={playerPos.y} width="32" height="32" />
+          <image href="/Idle.png" x={npcPos.x} y={npcPos.y} width="32" height="32" />
         </svg>
       </div>
     </>
-  )
+  );
 }
 
 export default App
